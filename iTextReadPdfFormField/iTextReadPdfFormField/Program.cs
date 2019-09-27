969974a47f1cbfcc14af88a581df5d7cd1ce4c61
Musa Balta <musa.balta@c-section.com>
2019-09-26 20:39:21 -0400

    Sep 26, 2019 iTextSharp included

--- iTextReadPdfFormField/iTextReadPdfFormField/Program.cs
+++ iTextReadPdfFormField/iTextReadPdfFormField/Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace iTextReadPdfFormField
 {
     class Program
     {
+        private static string GetFormFieldNames(PdfReader pdfReader)
+        {
+            return string.Join("\r\n", pdfReader.AcroFields.Fields.Select(x => x.Key).ToArray()); // return Field Name List
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string inputFile = "PdfDocument.pdf";
+
+            PdfReader pdfReader = new PdfReader(inputFile);
+
+            PdfReader.unethicalreading = true;
+
+            string outputFile = "OutputPdfFile.pdf";
+
+            PdfStamper pdfStamp = new PdfStamper(pdfReader, new FileStream(outputFile, FileMode.Create));
+
+            AcroFields fields = pdfStamp.AcroFields;
+
+            fields.SetField("Field Name", "Input Value"); // If you do not know *field name*, you can run *GetFormFieldNames(pdfReader)* function to learn
+
+            pdfStamp.Close();
+
+            pdfReader.Close();
         }
     }
 }
